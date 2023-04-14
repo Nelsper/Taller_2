2e3b00ce2d55c3fe0b48fa0010be2ca4ea6564ae
Nelsper <nelsper@gmail.com>
2023-04-13 21:40:45 -0500

    Ajustes del index y creacion del envento click del form

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World');
+const form = document.getElementById('form') as HTMLFormElement;//Captura del form
+const btnGuardar = document.getElementById('btn-guardar') as HTMLButtonElement;//Captura el boton guardar
+
+btnGuardar.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Ingreso a la validación del formulario');
+    //Validación del formulario
+
+    //Obtener los valores de los inputs (Capturar)
+    const tipoDni = document.getElementById('tipoDni') as HTMLSelectElement;
+    const numberDni = document.getElementById('numberDni') as HTMLInputElement;
+    const name = document.getElementById('name') as HTMLInputElement;
+    const lastName = document.getElementById('lastName') as HTMLInputElement;
+    const phone = document.getElementById('phone') as HTMLInputElement;
+    const email = document.getElementById('email') as HTMLInputElement;
+    const linkedin = document.getElementById('linkedin') as HTMLInputElement;
+    const github = document.getElementById('github') as HTMLInputElement;
+
+    console.log(tipoDni.value);
+});
